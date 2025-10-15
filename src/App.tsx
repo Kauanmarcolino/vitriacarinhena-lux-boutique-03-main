4483ce54df19fc497e8e5f5cde480b9918d6d53a
vitorhassis <vitorassis997@gmail.com>
2025-10-15 16:00:55 -0300

    corrigindo bug

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Catalog from "./pages/Catalog";
 import About from "./pages/About";
-import Video from "./pages/Video";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
@@ -26,7 +25,6 @@ const App = () => (
   <Route path="/" element={<Index />} />
   <Route path="/catalogo" element={<Catalog />} />
   <Route path="/sobre" element={<About />} />
-  <Route path="/video" element={<Video />} />
   <Route path="/contato" element={<Contact />} />
   <Route path="/produto/:id" element={<Product />} />
   <Route path="/favoritos" element={<Favorites />} />
